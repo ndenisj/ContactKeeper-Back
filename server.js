7bed3722d462e67e7dc64f0991b6ff07573d692d
Denis John <denis.john.ndarake@gmail.com>
2019-09-13 15:34:42 +0100

    Register new user and save in mongoDB and return json token using jwt

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to the contact keeper API..." });
 });
-
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
