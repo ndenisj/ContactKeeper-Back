2f9bf320255da9c434925cfffd03c9323a7c46ed
Denis John <denis.john.ndaraek@gmail.com>
2019-09-29 19:09:22 +0100

    Removed initial route

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  // res.json({ msg: "Welcome to the contact keeper API..." });
-});
+// app.get("/", (req, res) => {
+//   // res.json({ msg: "Welcome to the contact keeper API..." });
+// });
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
